15f6ea7735b2f27c284295fba261133d535949e9
june <kimjy4536@hotmail.com>
2019-01-20 19:16:43 +0900

    db 모델 구성

--- db/models/reminderlist.js
+++ db/models/reminderlist.js
@@ -1,10 +1,12 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   var ReminderList = sequelize.define('ReminderList', {
-    name: DataTypes.STRING
+    name: DataTypes.STRING,
+    created_at: DataTypes.DATE
   }, {});
   ReminderList.associate = function(models) {
     // associations can be defined here
+    ReminderList.hasMany(models.ReminderListItem);
   };
   return ReminderList;
 };
--- db/models/reminderlistitem.js
+++ db/models/reminderlistitem.js
@@ -1,10 +1,17 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   var ReminderListItem = sequelize.define('ReminderListItem', {
-    name: DataTypes.STRING
+    list_id: DataTypes.STRING,
+    name: DataTypes.STRING,
+    status: DataTypes.STRING,
+    created_at: DataTypes.DATE,
+    remind_at: DataTypes.DATE
   }, {});
   ReminderListItem.associate = function(models) {
     // associations can be defined here
+    ReminderListItem.belongsTo(models.ReminderList, {
+      foreignKey: 'list_id'
+    })
   };
   return ReminderListItem;
 };
